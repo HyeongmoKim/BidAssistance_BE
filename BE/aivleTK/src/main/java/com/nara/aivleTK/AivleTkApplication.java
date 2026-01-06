acb57a452ee48896c4a387ea42f85b01e496e261
HyeongmoKim <rga6581@gmail.com>
2026-01-06 15:49:44 +0900

    SpringBoot

---
+++ BE/aivleTK/src/main/java/com/nara/aivleTK/AivleTkApplication.java
@@ -0,0 +1,13 @@
+package com.nara.aivleTK;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AivleTkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AivleTkApplication.class, args);
+	}
+
+}
