f3f62b32321e1b06099fafdb4d66dba0a2a68a85
HyeongmoKim <rga6581@gmail.com>
2026-01-14 11:15:11 +0900

    에러 해결용

--- BE/aivleTK/src/main/java/com/nara/aivleTK/exception/DeletionException.java
+++ BE/aivleTK/src/main/java/com/nara/aivleTK/exception/DeletionException.java
@@ -4,7 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.FORBIDDEN)
-public class DeletionException extends RuntimeException {
+public class DeletionException extends RuntimeException  {
     public DeletionException(String message) {
         super(message);
     }
