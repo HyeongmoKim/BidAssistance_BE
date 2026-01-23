ecb4ae5bec1769d14279b7ea3fcd7693318549cf
HyeongmoKim <rga6581@gmail.com>
2026-01-23 16:48:17 +0900

    챗봇 컨트롤러 수정

--- BE/aivleTK/src/main/java/com/nara/aivleTK/controller/ChatBotController.java
+++ BE/aivleTK/src/main/java/com/nara/aivleTK/controller/ChatBotController.java
@@ -19,7 +19,7 @@ public class ChatBotController {
 
     @PostMapping
     public ResponseEntity<ApiResponse<ChatResponse>> chat(@RequestBody PythonChatRequest request){
-        ChatResponse response = chatBotService.getAiAnalysis(request.getPrompt());
+        ChatResponse response = chatBotService.getChatResponse(request.getPrompt());
         return ResponseEntity.ok(ApiResponse.success(response));
     }
 }
