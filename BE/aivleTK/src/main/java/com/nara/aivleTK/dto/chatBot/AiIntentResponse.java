80469505fd6803ce9a4af948f0eb623bc5fbd755
HyeongmoKim <rga6581@gmail.com>
2026-01-23 16:42:55 +0900

    챗봇 추가

---
+++ BE/aivleTK/src/main/java/com/nara/aivleTK/dto/chatBot/AiIntentResponse.java
@@ -0,0 +1,16 @@
+package com.nara.aivleTK.dto.chatBot;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+public class AiIntentResponse {
+    private String region;
+    private String keyword;
+    private String type;
+}
