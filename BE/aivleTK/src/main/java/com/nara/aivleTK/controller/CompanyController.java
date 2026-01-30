47ccd025d2827e8439f943b0d7cdc8795b72b264
pla2n <rred5899@gmail.com>
2026-01-30 11:18:02 +0900

    company 오타 수정

--- BE/aivleTK/src/main/java/com/nara/aivleTK/controller/CompanyController.java
+++ BE/aivleTK/src/main/java/com/nara/aivleTK/controller/CompanyController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/compnay")
+@RequestMapping("/api/company")
 @RequiredArgsConstructor
 public class CompanyController {
     private final CompanyService companyService;
