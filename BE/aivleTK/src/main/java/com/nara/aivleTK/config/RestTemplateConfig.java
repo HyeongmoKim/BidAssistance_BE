5053c6dd691ea604b2cd1250c73c9926a58e6371
HyeongmoKim <rga6581@gmail.com>
2026-02-04 15:25:55 +0900

    rest템플릿 수정

--- BE/aivleTK/src/main/java/com/nara/aivleTK/config/RestTemplateConfig.java
+++ BE/aivleTK/src/main/java/com/nara/aivleTK/config/RestTemplateConfig.java
@@ -2,8 +2,12 @@ package com.nara.aivleTK.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
 import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.Arrays;
 
 @Configuration
 public class RestTemplateConfig {
@@ -12,6 +16,16 @@ public class RestTemplateConfig {
         SimpleClientHttpRequestFactory requestFactory = new SimpleClientHttpRequestFactory();
         requestFactory.setConnectTimeout(5000);
         requestFactory.setReadTimeout(300000);
-        return new RestTemplate(requestFactory);
+        RestTemplate restTemplate = new RestTemplate(requestFactory);
+
+// 2. [추가] "octet-stream"도 JSON으로 인식하게 만드는 컨버터 추가
+        MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
+        converter.setSupportedMediaTypes(Arrays.asList(
+                MediaType.APPLICATION_JSON,
+                MediaType.APPLICATION_OCTET_STREAM // 이 부분이 핵심입니다!
+        ));
+        restTemplate.getMessageConverters().add(0, converter); // 우선순위 높게 추가
+
+        return restTemplate;
     }
 }
