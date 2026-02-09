08b767cc297ac9817ab35bb7f69b8a1c9d272326
HyeongmoKim <rga6581@gmail.com>
2026-02-09 15:06:27 +0900

    쿠키 변경

---
+++ BE/aivleTK/src/main/java/com/nara/aivleTK/config/CookieConfig.java
@@ -0,0 +1,12 @@
+import org.springframework.boot.web.servlet.server.CookieSameSiteSupplier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class CookieConfig {
+
+    @Bean
+    public CookieSameSiteSupplier cookieSameSiteSupplier() {
+        return CookieSameSiteSupplier.ofNone();
+    }
+}
