ee4af9ede358cfe150ed9c2db8cc0703f491ae48
pla2n <rred5899@gmail.com>
2026-02-10 17:24:13 +0900

    비드 로그 수정

--- BE/aivleTK/src/main/java/com/nara/aivleTK/domain/BidLog.java
+++ BE/aivleTK/src/main/java/com/nara/aivleTK/domain/BidLog.java
@@ -30,4 +30,8 @@ public class BidLog {
     @Column(nullable = false)
     private LocalDateTime date; // 입찰 일시
 
+    @Builder.Default
+    @Column(nullable = false)
+    private Long price = 0L; // DB에 NOT NULL로 존재, 조회 로그에는 0 기본값
+
 }
